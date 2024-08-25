7b370978a5839cecb5a91d4a0e6cdf3f07e0c303
Jorge <jomruizgo@gmail.com>
2024-08-25 11:37:55 -0500

    first commit

---
+++ src/main/java/com/emazon/msvc_stock/MsvcStockApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.msvc_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcStockApplication.class, args);
+	}
+
+}
